d627830fc246eec9e08e901e996f09daf05c9da9
Mohan Dandigam <mohandand@gmail.com>
2022-06-17 15:16:06 -0700

    Updated Code for My Portfoio roject

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
     header.classList.toggle("active", window.scrollY > 0);
   });
   // Toogle Menu
-   const [show, setShow] = useState(true);
+   const [show, setShow] = useState(false);
   return (
     <div className="home" id="Home">
       <div className="home__bg">
